fc94fbf7b732a46d0a0433c968315fc587acff83
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-08-31 09:41:17 -0400

    Add igw

--- main.tf
+++ main.tf
@@ -6,6 +6,16 @@ resource "aws_vpc" "cloud" {
   }
 }
 
+# Cloud VPC IGW
+resource "aws_internet_gateway" "cloud" {
+  vpc_id = aws_vpc.cloud.id
+
+  tags = {
+    Name = var.cloud_vpc_name
+  }
+}
+
+
 # OnPrem VPC
 resource "aws_vpc" "onprem" {
   cidr_block = var.onprem_vpc_cidr
@@ -13,3 +23,13 @@ resource "aws_vpc" "onprem" {
     Name = var.onprem_vpc_name
   }
 }
+
+# OnPrem VPC IGW
+resource "aws_internet_gateway" "onprem" {
+  vpc_id = aws_vpc.onprem.id
+
+  tags = {
+    Name = var.onprem_vpc_name
+  }
+}
+
