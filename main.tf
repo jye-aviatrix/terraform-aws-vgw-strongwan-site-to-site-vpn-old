03bc84515437667db664d324132203b19c6c6146
jye-aviatrix <93721751+jye-aviatrix@users.noreply.github.com>
2022-08-31 11:28:27 -0400

    add on-prem vpc

--- main.tf
+++ main.tf
@@ -12,3 +12,18 @@ module "cloudvpc" {
   remote_vpc_cidr    = var.onprem_vpc_cidr
 
 }
+
+
+
+module "onpremvpc" {
+  source = "./moduels/aws_vpc"
+
+  name = var.onprem_vpc_name
+  cidr = var.onprem_vpc_cidr
+
+  azs                = slice(data.aws_availability_zones.available.names, 0, 2) # Select first two aws availability zones
+  public_subnets     = slice(cidrsubnets(var.onprem_vpc_cidr, 2, 2, 2, 2), 0, 2) # Caculate consecuitive CIDR range for public subnets
+  private_subnets    = slice(cidrsubnets(var.onprem_vpc_cidr, 2, 2, 2, 2), 2, 4) # Caculate consecuitive CIDR range for private subnets
+  enable_vpn_gateway = false
+
+}
